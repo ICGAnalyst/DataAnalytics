574d1e6d89deb17a4d06327bf4520d90c1942a57
sakura-hly <1612584677@qq.com>
2018-08-14 10:20:18 +0800

    SearchSymbol

---
+++ DataAnalytics/Controllers/SearchSymbolController.cs
@@ -0,0 +1,31 @@
+﻿using DataAnalytics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnalytics.Controllers
+{
+    public class SearchSymbolController : Controller
+    {
+        // GET: SearchSymbol
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult GetSearchSymbol(string searchKey)
+        {
+            SearchSymbolBusiness searchSymbolBusiness = new SearchSymbolBusiness();
+            List<string> res = searchSymbolBusiness.GetSearchSymbol(searchKey);
+            var jsonObject = new
+            {
+                len = res.Count,
+                data = res
+            };
+            return Json(jsonObject);
+        }
+    }
+}
---
+++ DataAnalytics/Models/SearchSymbolBusiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAnalytics.Models
+{
+    public class SearchSymbolBusiness
+    {
+        private static DBDataContext db = new DBDataContext();
+
+        public List<string> GetSearchSymbol(string searchKey)
+        {
+            List<string> searchSymbols = new List<string>();
+            try
+            {
+                searchSymbols = (from s in db.symbols
+                                 where s.symbol.StartsWith(searchKey)
+                                 select s.symbol).ToList();
+                return searchSymbols;
+            }
+            catch (Exception e)
+            {
+                //
+                return searchSymbols;
+            }
+        }
+    }
+}
