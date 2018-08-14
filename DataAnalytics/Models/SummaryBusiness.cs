93340ecd84603464d7d89082a0e22b7739cd7ea7
sakura-hly <1612584677@qq.com>
2018-08-14 16:10:23 +0800

    Summary Graph

---
+++ DataAnalytics/Models/SummaryBusiness.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAnalytics.Models
+{
+    public class SummaryBusiness
+    {
+        private static DBDataContext db = new DBDataContext();
+
+        public List<SummaryData> GetSummaryData(string symbol)
+        {
+            List<SummaryData> summaryDatas = new List<SummaryData>();
+            try { 
+                var resultSet = (from a in db.AggsByHour
+                                 join s in db.symbols
+                                 on a.symbolID equals s.symbolID
+                                 where s.symbol == symbol
+                                 select new { a.Date, a.Time, a.Open, a.High, a.Low, a.Close, a.Volume }).ToList();
+                foreach (var item in resultSet)
+                {
+                    SummaryData summaryData = new SummaryData();
+                    summaryData.Open = (decimal)item.Open;
+                    summaryData.High = (decimal)item.High;
+                    summaryData.Low = (decimal)item.Low;
+                    summaryData.Close = (decimal)item.Close;
+                    summaryData.Volume = (float)item.Volume;
+                    summaryDatas.Add(summaryData);
+                }
+                return summaryDatas;
+            }
+            catch (Exception e)
+            {
+                return summaryDatas;
+            }
+        }
+    }
+}
---
+++ DataAnalytics/Models/SummaryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataAnalytics.Models
+{
+    public class SummaryData
+    {
+        private int dateTime;
+
+        private decimal open;
+
+        private decimal high;
+
+        private decimal low;
+
+        private decimal close;
+
+        private float volume;
+
+        public int DateTime { get => dateTime; set => dateTime = value; }
+        public decimal Open { get => open; set => open = value; }
+        public decimal High { get => high; set => high = value; }
+        public decimal Low { get => low; set => low = value; }
+        public decimal Close { get => close; set => close = value; }
+        public float Volume { get => volume; set => volume = value; }
+    }
+}
