417674bf1e56d5a1f91a4072b6f2d2873dbe9384
sakura-hly <1612584677@qq.com>
2018-08-14 23:28:16 +0800

    fix the Autocomplete Input

--- DataAnalytics/Models/HistoricBusiness.cs
+++ DataAnalytics/Models/HistoricBusiness.cs
@@ -10,54 +10,61 @@ namespace DataAnalytics.Models
         public List<Historic> GetHisToricData(string symbolKey, int fromDate, int toDate)
         {
             List<Historic> historics = new List<Historic>();
-            var db = new DBDataContext();
-
-            var symbolIds = (from s in db.symbols
-                             where s.symbol == symbolKey
-                             select s.symbolID).ToList();
-            int symbolId = symbolIds[0];
-            var resHour = (from s in db.AggsByHour
-                           where s.symbolID == symbolId
-                                 && s.Date >= fromDate
-                                 && s.Date <= toDate
-                           select new { s.ID, s.Date, s.Open, s.High, s.Low, s.Close, s.Volume }).ToList();
-
-            int count = 0;
-            int len = resHour.Count();
-            for (count = 0; count < len;)
+            try
             {
-                int curDate = (int)resHour[count].Date;
-                decimal open = 0, high = 0, low = 0, close = 0;
-                long volume = 0;
-                for (int i = 0; i < 7; i++, count++)
+                var db = new DBDataContext();
+
+                var symbolIds = (from s in db.symbols
+                                 where s.symbol == symbolKey
+                                 select s.symbolID).ToList();
+                int symbolId = symbolIds[0];
+                var resHour = (from s in db.AggsByHour
+                               where s.symbolID == symbolId
+                                     && s.Date >= fromDate
+                                     && s.Date <= toDate
+                               select new { s.ID, s.Date, s.Open, s.High, s.Low, s.Close, s.Volume }).ToList();
+
+                int count = 0;
+                int len = resHour.Count();
+                for (count = 0; count < len;)
                 {
-                    if (i == 0)
+                    int curDate = (int)resHour[count].Date;
+                    decimal open = 0, high = 0, low = 0, close = 0;
+                    long volume = 0;
+                    for (int i = 0; i < 7; i++, count++)
                     {
-                        open = (decimal)resHour[count].Open;
-                    }
-                    if (i == 6)
-                    {
-                        close = (decimal)resHour[count].Close;
-                    }
+                        if (i == 0)
+                        {
+                            open = (decimal)resHour[count].Open;
+                        }
+                        if (i == 6)
+                        {
+                            close = (decimal)resHour[count].Close;
+                        }
 
-                    high = (decimal)resHour[count].High > high ? (decimal)resHour[count].High : high;
-                    low = (decimal)resHour[count].Low > low ? (decimal)resHour[count].Low : low;
-                    volume += (long)resHour[count].Volume;
+                        high = (decimal)resHour[count].High > high ? (decimal)resHour[count].High : high;
+                        low = (decimal)resHour[count].Low > low ? (decimal)resHour[count].Low : low;
+                        volume += (long)resHour[count].Volume;
+                    }
+                    Historic historic = new Historic();
+                    historic.SymbolID = symbolId;
+                    historic.Date = resHour[count - 7].Date;
+                    historic.Symbol = symbolKey;
+                    historic.Open = open;
+                    historic.High = high;
+                    historic.Low = low;
+                    historic.Close = close;
+                    historic.Volume = volume;
+
+                    historics.Add(historic);
                 }
-                Historic historic = new Historic();
-                historic.SymbolID = symbolId;
-                historic.Date = resHour[count - 7].Date;
-                historic.Symbol = symbolKey;
-                historic.Open = open;
-                historic.High = high;
-                historic.Low = low;
-                historic.Close = close;
-                historic.Volume = volume;
-
-                historics.Add(historic);
+                //System.Console.WriteLine(historics);
+                return historics;
             }
-            System.Console.WriteLine(historics);
-            return historics;
+            catch (Exception e)
+            {
+                return historics;
+            }
         }
     }
 }
